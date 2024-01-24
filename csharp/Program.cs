c92645d681c3b92509bab35b925602af71cb8a2e
muhammad <mukhamed.saidov.2002@mail.ru>
2024-01-24 10:20:27 +0500

    1 коммит

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+using static System.Console; 
+Clear(); 
+ 
+ 
+ 
+Write("Введите количество элементов массива: "); 
+int m = Convert.ToInt32(ReadLine()); 
+string [] stringArray = new string [m]; 
+void array(string [] stringArray) 
+{ 
+  for (int i = 0;i<stringArray.Length;i++) 
+  { 
+     WriteLine($"Введите {i+1} элемент массива"); 
+     stringArray[i] = ReadLine(); 
+  } 
+} 
